86da626f78f8e3b88fc7d3c2a705460df6dece12
wangxl <i0air@qq.com>
2024-09-11 17:45:55 +0800

    环境准备

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-project
+# vue-fabric
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    origin: '0.0.0.0',
+    port: 8000,
+    host: true
   }
 })
